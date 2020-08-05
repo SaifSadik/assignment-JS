5a2a0f063152bb3f2bd61c42dc3e33143f2a0bed
Khandoker Saif Sadik <53291528+SaifSadik@users.noreply.github.com>
2020-08-05 10:19:37 +0600

    woodCalculator done

--- assignment.js
+++ assignment.js
@@ -1,10 +1,22 @@
-function feetToMile(feet) {
-	if (feet < (0)) {
-		return 'input cannot be less than zero'; 
-	} else {
-		return (feet * 0.000189394);
-	}
+                         // Assignment-01- feetToMile
+
+// function feetToMile(feet) {
+// 	if (feet < 0) {
+// 		return 'input cannot be less than zero'; 
+// 	} else {
+// 		return (feet * 0.000189394);
+// 	}
+// }
+
+// var result = feetToMile(5280);
+// console.log("Mile is: ", result);
+
+
+                                    // Assignment-02-woodCalculator
+
+function woodCalculator(chair, table, bed){
+	return chair + table*3 + bed*5;
 }
 
-var result = feetToMile(5280);
-console.log(result);
+var result = woodCalculator(10, 4, 2);
+console.log(result, "cubic feet wood will be Needed.");
